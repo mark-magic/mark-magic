cd2d75a6925756af1c87630d66beff5403b88a9a
rxliuli <rxliuli@gmail.com>
2022-10-11 22:39:21 +0800

    feat: 为 @mami/cli 添加一些基本的输出

--- packages/cli/src/bin.ts
+++ packages/cli/src/bin.ts
@@ -1,5 +1,6 @@
 import { pathExists } from '@liuli-util/fs-extra'
 import { bundleRequire } from 'bundle-require'
+import ora from 'ora'
 import path from 'path'
 import { convert } from './convert'
 
@@ -10,4 +11,11 @@ if (!(await pathExists(configPath))) {
 const { mod } = await bundleRequire({
   filepath: configPath,
 })
-await convert(mod.default)
+console.log('start')
+const spinner = ora().start()
+spinner.color = 'blue'
+await convert(mod.default).on('generate', ({ note }) => {
+  spinner.text = 'handle: ' + note.title
+})
+spinner.stop()
+console.log('end')
--- packages/cli/src/convert.ts
+++ packages/cli/src/convert.ts
@@ -1,4 +1,5 @@
 import { groupBy } from 'lodash-es'
+import { PromiseUtil } from './utils/PromiseUtil'
 
 export interface Tag {
   id: string
@@ -38,26 +39,35 @@ export interface ConvertConfig {
   plugins: (InputPlugin | OutputPlugin)[]
 }
 
-export async function convert(options: ConvertConfig) {
-  const p = groupBy(options.plugins, (item) => 'generate' in item)
-  const inputs = (p['true'] ?? []) as InputPlugin[]
-  const outputs = (p['false'] ?? []) as OutputPlugin[]
-  if (inputs.length > 0) {
-    for (const output of outputs) {
-      await output.config?.(options)
-    }
-  }
-  for (const input of inputs) {
-    const generator = input.generate()
-    for await (const note of generator) {
+export interface Events {
+  generate?(options: { input: InputPlugin; note: Note }): void
+  handle?(options: { input: InputPlugin; output: OutputPlugin; note: Note }): void
+}
+
+export function convert(options: ConvertConfig) {
+  return PromiseUtil.warpOnEvent(async (events: Events) => {
+    const p = groupBy(options.plugins, (item) => 'generate' in item)
+    const inputs = (p['true'] ?? []) as InputPlugin[]
+    const outputs = (p['false'] ?? []) as OutputPlugin[]
+    if (inputs.length > 0) {
       for (const output of outputs) {
-        await output.handle(note)
+        await output.config?.(options)
       }
     }
-  }
-  if (inputs.length > 0) {
-    for (const output of outputs) {
-      await output.end?.()
+    for (const input of inputs) {
+      const generator = input.generate()
+      for await (const note of generator) {
+        events.generate?.({ input, note })
+        for (const output of outputs) {
+          events.handle?.({ input, output, note })
+          await output.handle(note)
+        }
+      }
     }
-  }
+    if (inputs.length > 0) {
+      for (const output of outputs) {
+        await output.end?.()
+      }
+    }
+  })
 }

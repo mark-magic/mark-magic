6b7b6647f7909d3f4584f94d4535b6d03e7fec86
rxliuli <rxliuli@gmail.com>
2022-10-14 19:25:59 +0800

    feat: 更新示例配置

--- demos/joplin-hexo-demo/mami.config.ts
+++ demos/joplin-hexo-demo/mami.config.ts
@@ -3,13 +3,13 @@ import { joplinInput } from '@mami/plugin-joplin-input'
 import { hexoOutput } from '@mami/plugin-hexo-output'
 
 export default defineConfig({
-  plugins: [
+  input: [
     joplinInput({
       baseUrl: 'http://localhost:27583',
       token:
         '5bcfa49330788dd68efea27a0a133d2df24df68c3fd78731eaa9914ef34811a34a782233025ed8a651677ec303de6a04e54b57a27d48898ff043fd812d8e0b31',
       tag: 'blog',
     }),
-    hexoOutput(),
   ],
+  output: [hexoOutput()],
 })

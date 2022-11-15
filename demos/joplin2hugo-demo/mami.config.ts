11072ef5c901fe69faae2134125645ef4abf0db6
rxliuli <rxliuli@gmail.com>
2022-11-15 09:54:50 +0800

    feat: 更新 joplin2hugo，将 theme 保存到当前项目中

--- demos/joplin2hugo-demo/mami.config.ts
+++ demos/joplin2hugo-demo/mami.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from '@mami/cli'
 import * as joplin from '@mami/plugin-joplin'
-import { hugoOutput } from '@mami/plugin-hugo'
+import * as hugo from '@mami/plugin-hugo'
 
 export default defineConfig({
   input: [
     joplin.input({
-      baseUrl: 'http://localhost:27583',
+      baseUrl: 'http://127.0.0.1:27583',
       token:
         '5bcfa49330788dd68efea27a0a133d2df24df68c3fd78731eaa9914ef34811a34a782233025ed8a651677ec303de6a04e54b57a27d48898ff043fd812d8e0b31',
       tag: 'blog',
     }),
   ],
-  output: [hugoOutput()],
+  output: [hugo.output()],
 })

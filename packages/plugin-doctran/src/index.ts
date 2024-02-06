43d0f483783759598c9e046f4bdc1d5dd0572c83
rxliuli <rxliuli@gmail.com>
2024-02-07 02:43:12 +0900

    refactor(plugin-doctran): 使用基于文件名的缓存模式

--- packages/plugin-doctran/src/index.ts
+++ packages/plugin-doctran/src/index.ts
@@ -1,41 +1,44 @@
 import { TransformPlugin } from '@mark-magic/core'
 import { createTrans, trans } from './translate'
 import findCacheDirectory from 'find-cache-dir'
-import { mkdir, writeFile } from 'fs/promises'
+import { mkdir } from 'fs/promises'
 import path from 'pathe'
-import { pathExists, readJson } from 'fs-extra/esm'
+import { pathExists, readJson, writeJson } from 'fs-extra/esm'
 import { DoctranTransformConfig } from './config.schema'
 
 export function transform(options: DoctranTransformConfig): TransformPlugin {
-  const map: Record<string, string> = {}
-  const dir = findCacheDirectory({ name: '@mark-magic/plugin-doctran' })
+  const cacheDir = findCacheDirectory({ name: '@mark-magic/plugin-doctran' })!
+
   return {
     name: 'doctran',
     async start() {
-      if (!dir) {
-        return
+      if (!cacheDir) {
+        throw new Error('Cannot find cache directory')
       }
-      const cachePath = path.resolve(dir, 'cache.json')
-      if (await pathExists(cachePath)) {
-        Object.assign(map, await readJson(cachePath))
-      }
+      await mkdir(cacheDir, { recursive: true })
     },
     async transform(content) {
       const origin = content.content
-      if (map[origin]) {
-        content.content = map[origin]
-        return content
+      const cachePath = path.resolve(cacheDir, `${content.id}.json`)
+
+      interface Cache {
+        origin: string
+        transformed: string
+      }
+
+      if (await pathExists(cachePath)) {
+        const cache = (await readJson(cachePath)) as Cache
+        if (cache.origin === origin) {
+          content.content = cache.transformed
+          return content
+        }
       }
       content.content = await trans(createTrans(options), origin)
-      map[origin] = content.content
+      await writeJson(cachePath, {
+        origin,
+        transformed: content.content,
+      } as Cache)
       return content
-    },
-    async end() {
-      if (!dir) {
-        return
-      }
-      await mkdir(dir, { recursive: true })
-      await writeFile(path.resolve(dir, 'cache.json'), JSON.stringify(map, null, 2))
     },
   }
 }
